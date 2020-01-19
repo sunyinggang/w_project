e1483a6666ba4cb4cfbe2bc10b0130a21936adb6
sunyinggang <136080416@qq.com>
2020-01-19 21:26:38 +0800

    autscore教务端完成

--- python/autscore/app/admin/forms.py
+++ python/autscore/app/admin/forms.py
@@ -1,3 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
+
+class ClassForm(FlaskForm):
+    name = StringField(
+        label="班级名",
+        validators=[
+            DataRequired("班级名不为空！")
+        ],
+        description="班级名",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "格式：系别-年级-班级（如：软件-16-3）"
+        }
+    )
+    submit = SubmitField(
+        "确认",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
 
 
--- python/autscore/app/admin/views.py
+++ python/autscore/app/admin/views.py
@@ -1,17 +1,52 @@
 import os
 
-from flask import render_template, send_from_directory, make_response, send_file, request
+import xlrd
+from flask import render_template, send_from_directory, make_response, send_file, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash
+from app import db
 
 import app
 from . import admin
+from .forms import ClassForm
+from ..models import Teacher, Student, Class
+
 
 @admin.route("/")
 def index():
     return render_template("admin/admin.html")
 
-@admin.route("/aclass/")
-def aclass():
-    return render_template("admin/aclass.html")
+@admin.route("/aclass/<int:page>/",methods=["GET"])
+def aclass(page=None):
+    if page is None:
+        page = 1
+    class_list = Class.query.paginate(page=page,per_page=5)
+    return render_template("admin/aclass.html",class_list = class_list)
+
+@admin.route("/aclass/add", methods=['POST', 'GET'])
+def classAdd():
+    form = ClassForm()
+    if form.validate_on_submit():
+        data = form.data
+        aclass = Class.query.filter_by(name=data["name"]).count()
+        if aclass == 1:
+            flash("班级名已存在!")
+            return redirect(url_for("admin.classAdd"))
+        aclass = Class(
+            name=data["name"]
+        )
+        db.session.add(aclass)
+        db.session.commit()
+        flash("添加成功！")
+        return redirect(url_for("admin.aclass",page=1))
+    return render_template("admin/addclass.html",form=form)
+
+@admin.route("/aclass/del/<id>/",methods=["GET","POST"])
+def classDel(id=None):
+    aclass = Class.query.filter_by(id=id).first_or_404()
+    db.session.delete(aclass)
+    db.session.commit()
+    flash("删除成功！")
+    return redirect(url_for('admin.aclass',page=1))
 
 @admin.route("/ainput/")
 def ainput():
@@ -25,35 +60,121 @@ def download():
     return response
 
 @admin.route("/fileo/", methods=['POST', 'GET'])
-def fileo():
-    print(request.files)
+def fileOne():
     file = request.files['fileo']
-    print('file', type(file), file)
-    print(file.filename)    # 打印文件名
-    #
-    # f = file.read()    #文件内容
-    # data = xlrd.open_workbook(file_contents=f)
-    # table = data.sheets()[0]
-    # names = data.sheet_names()  # 返回book中所有工作表的名字
-    # status = data.sheet_loaded(names[0])  # 检查sheet1是否导入完毕
-    # print(status)
-    # nrows = table.nrows  # 获取该sheet中的有效行数
-    # ncols = table.ncols  # 获取该sheet中的有效列数
-    # print(nrows)
-    # print(ncols)
-    # s = table.col_values(0)  # 第1列数据
-    # for i in s:
-    #     ii = i.strip()
-    #     print(len(ii))
-    return 'OK'
+    f = file.read()    #文件内容
+    data = xlrd.open_workbook(file_contents=f)
+    table = data.sheets()[0]
+    nrows = table.nrows  # 获取该sheet中的有效行数
+    ncols = table.ncols  # 获取该sheet中的有效列数
+    list = []
+    for i in range(nrows):
+        rowlist = []
+        for j in range(ncols):
+            rowlist.append(table.cell_value(i, j))
+        list.append(rowlist)
+    del list[0]  # 删掉第一行，第一行获取的是文件的头，一般不用插到数据库里面
 
-@admin.route("/astudent/")
-def astudent():
-    return render_template("admin/astudent.html")
+    for a in list:
+        teachers = Teacher()
+        teachers.username = a[0]
+        teachers.name = a[1]
+        teachers.password = generate_password_hash("123456")
+        db.session.add(teachers)
+        db.session.commit()
+    flash("导入成功！")
+    return redirect(url_for('admin.ateacher'))
 
-@admin.route("/ateacher/")
-def ateacher():
-    return render_template("admin/ateacher.html")
+@admin.route("/filet/", methods=['POST', 'GET'])
+def fileTow():
+    file = request.files['filet']
+    f = file.read()    #文件内容
+    data = xlrd.open_workbook(file_contents=f)
+    table = data.sheets()[0]
+    nrows = table.nrows  # 获取该sheet中的有效行数
+    ncols = table.ncols  # 获取该sheet中的有效列数
+    list = []
+    for i in range(nrows):
+        rowlist = []
+        for j in range(ncols):
+            rowlist.append(table.cell_value(i, j))
+        list.append(rowlist)
+    del list[0]  # 删掉第一行，第一行获取的是文件的头，一般不用插到数据库里面
+
+    for a in list:
+        students = Student()
+        students.username = a[0]
+        students.name = a[1]
+        students.password = generate_password_hash("123456")
+        db.session.add(students)
+        db.session.commit()
+    flash("导入成功！")
+    return redirect(url_for('admin.astudent'))
+
+@admin.route("/astudent/<int:page>/",methods=["GET"])
+def astudent(page=None):
+    if page is None:
+        page = 1
+    student_list = Student.query.paginate(page=page,per_page=5)
+    return render_template("admin/astudent.html",student_list=student_list)
+
+@admin.route("/astudent/del/<id>/",methods=["GET","POST"])
+def studentDel(id=None):
+    student = Student.query.filter_by(id=id).first_or_404()
+    db.session.delete(student)
+    db.session.commit()
+    flash("删除成功！")
+    return redirect(url_for('admin.astudent'))
+
+@admin.route("/astudent/search/")
+def studentSearch():
+    username = request.args.get("username")
+    student_list = Student.query
+    if username:
+        student_list = student_list.filter(Student.username==username)
+    return render_template("admin/astudent.html",student_list = student_list)
+
+@admin.route("/astudent/edit/<id>/",methods=["GET","POST"])
+def studentEdit(id=None):
+    student = Student.query.filter_by(id=id).first_or_404()
+    student.password = generate_password_hash("123456")
+    db.session.add(student)
+    db.session.commit()
+    flash("修改成功")
+    return redirect(url_for("admin.astudent"))
+
+
+@admin.route("/ateacher/<int:page>/",methods=["GET"])
+def ateacher(page=None):
+    if page is None:
+        page = 1
+    teacher_list = Teacher.query.paginate(page=page,per_page=5)
+    return render_template("admin/ateacher.html",teacher_list = teacher_list)
+
+@admin.route("/ateacher/del/<id>/",methods=["GET","POST"])
+def ateacherDel(id=None):
+    teacher = Teacher.query.filter_by(id=id).first_or_404()
+    db.session.delete(teacher)
+    db.session.commit()
+    flash("删除成功！")
+    return redirect(url_for('admin.ateacher'))
+
+@admin.route("/ateacher/search/")
+def teacherSearch():
+    username = request.args.get("username")
+    teacher_list = Teacher.query
+    if username:
+        teacher_list = teacher_list.filter(Teacher.username==username)
+    return render_template("admin/ateacher.html",teacher_list = teacher_list)
+
+@admin.route("/ateacher/edit/<id>/",methods=["GET","POST"])
+def teacherEdit(id=None):
+    teacher = Teacher.query.filter_by(id=id).first_or_404()
+    teacher.password = generate_password_hash("123456")
+    db.session.add(teacher)
+    db.session.commit()
+    flash("修改成功")
+    return redirect(url_for("admin.ateacher"))
 
 
 
