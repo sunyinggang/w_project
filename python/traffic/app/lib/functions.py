013d2b87aeed9e4c1fa4593e2692f0d6ef7edf32
sunyinggang <136080416@qq.com>
2020-03-25 20:52:29 +0800

    bishe

--- python/traffic/app/lib/functions.py
+++ python/traffic/app/lib/functions.py
@@ -3,15 +3,16 @@ import uuid
 import datetime
 import requests
 import json
-#修改文件名
 
 
+#  修改文件名
 def change_filename(filename):
     fileinfo = os.path.splitext(filename)
     filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")+str(uuid.uuid4().hex)+fileinfo[-1]
     return filename
 
-#模拟post/get请求
+
+#  模拟post/get请求
 def curl(url, params, type):
     baseUrl = 'https://tsapi.amap.com/v1/track/'
     url = baseUrl + url
@@ -22,5 +23,24 @@ def curl(url, params, type):
     else:
         response = requests.post(url, params)
     res = json.loads(response.content)
-    print(res)
+    return res
+
+
+# 地点转为经纬度
+def geocode_curl(address):
+    params = {}
+    url = 'https://restapi.amap.com/v3/geocode/geo'
+    params["address"] = address
+    params["key"] = '01b6e8c78f8f2d7ee079d0a51c88ba50'
+    response = requests.get(url, params)
+    res = json.loads(response.content)
+    return res['geocodes'][0]['location']
+
+
+# 驾车路径规划
+def driving_curl(params):
+    url = 'https://restapi.amap.com/v3/direction/driving'
+    params["key"] = '01b6e8c78f8f2d7ee079d0a51c88ba50'
+    response = requests.get(url, params)
+    res = json.loads(response.content)
     return res
--- python/traffic/test.py
+++ python/traffic/test.py
@@ -1,5 +1,55 @@
+import os
+import re
+import uuid
+import datetime
+import requests
+import json
 
-s = [(0,'无')]
-s+=([(1,'有')])
-print(s)
+#模拟post/get请求
+def curl(url, params, type):
+    baseUrl = 'https://restapi.amap.com/'
+    url = baseUrl + url
+    params["key"] = '01b6e8c78f8f2d7ee079d0a51c88ba50'
+    if type == 'GET':
+        response = requests.get(url, params)
+    else:
+        response = requests.post(url, params)
+    res = json.loads(response.text)
+    print(res)
+    return res
 
+def geocode_curl(address):
+    params = {}
+    url = 'https://restapi.amap.com/v3/geocode/geo'
+    params["address"] = address
+    params["key"] = '01b6e8c78f8f2d7ee079d0a51c88ba50'
+    response = requests.get(url, params)
+    res = json.loads(response.content)
+    return res['geocodes'][0]['location']
+
+def driving_curl(params):
+    url = 'https://restapi.amap.com/v3/direction/driving'
+    params["key"] = '01b6e8c78f8f2d7ee079d0a51c88ba50'
+    response = requests.get(url, params)
+    res = json.loads(response.content)
+    return res
+
+# params = {}
+# url = 'v3/direction/driving?origin=116.964624,36.614668&destination=116.483038,39.990633'
+# t = curl(url, params, 'GET')
+# print(t)
+# t = geocode_curl('北京市朝阳区阜通东大街6号')
+# print(t)
+params = {
+    'origin' : '116.964624,36.614668',
+    'destination' : '116.483038,39.990633'
+}
+t = driving_curl(params)
+k = t['route']['paths'][0]['steps']
+paths = [['116.964624','36.614668']]
+for m in k:
+    pp = m['polyline'].split(';')[0].split(',')
+    print(pp)
+    paths.append(pp)
+paths.append(['116.48303839','39.990633'])
+print(paths)
